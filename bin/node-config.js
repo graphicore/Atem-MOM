4417f50558f35095410f42d76b77ed4204339717
Lasse Fister <commander@graphicore.de>
2016-04-05 03:58:19 +0200

    Fix baseUrl when cli is used from outside of the project path.

--- bin/node-config.js
+++ bin/node-config.js
@@ -1,12 +1,13 @@
 define(['../lib/bower_components/Atem-RequireJS-Config/nodeConfig'],
 function(configure) {
-    var setup = {
-        baseUrl: 'lib'
-      , bowerPrefix: 'bower_components'
-      , paths: {
-            'Atem-MOM': './'
+    var path = require('path')
+      , setup = {
+            baseUrl: path.dirname(process.mainModule.filename) + '/../lib'
+          , bowerPrefix: 'bower_components'
+          , paths: {
+                'Atem-MOM': './'
+            }
         }
-    };
-
+      ;
     return configure.bind(null, setup);
 });
